49e62651e066fe91a9227f5952c41e7093fa446f
zyla303 <zyla303@hotmail.com>
2023-05-27 13:35:20 +0200

    Project Start
    Tetromino logic
    Camera movement
    Second block event

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Transform targetElement;
+    public GameObject cameraTrigger;
+    public GameObject tetrisManager;
+    public string tetrominoTag = "Tetromino";
+
+    private float cameraSpeed = 5f;
+    private bool isMovingCamera;
+    private Vector3 initialCameraPosition;
+    private Vector3 targetCameraPosition;
+
+    private void Start()
+    {
+        initialCameraPosition = cameraTransform.position;
+    }
+
+    private void Update()
+    {
+        if (IsTetrominoTouchingTrigger() && !IsTouchingTetrominoMoving())
+        {
+            if (!isMovingCamera)
+            {
+                isMovingCamera = true;
+                targetCameraPosition = cameraTransform.position + Vector3.up * cameraTrigger.transform.localScale.y;
+            }
+        }
+
+        if (isMovingCamera)
+        {
+            MoveCamera();
+            MoveTargetElement();
+            MoveCameraTrigger();
+            MoveTetrisManager();
+        }
+    }
+
+    private bool IsTetrominoTouchingTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(cameraTrigger.transform.position, cameraTrigger.transform.localScale, 0f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag(tetrominoTag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsTouchingTetrominoMoving()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(cameraTrigger.transform.position, cameraTrigger.transform.localScale, 0f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag(tetrominoTag))
+            {
+                Rigidbody2D tetrominoRigidbody = collider.GetComponent<Rigidbody2D>();
+                if (tetrominoRigidbody != null && tetrominoRigidbody.velocity.y < 0.000001)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void MoveCamera()
+    {
+        Vector3 targetPosition = cameraTransform.position + Vector3.up * cameraSpeed * Time.deltaTime;
+        cameraTransform.position = Vector3.MoveTowards(cameraTransform.position, targetPosition, cameraSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(cameraTransform.position, initialCameraPosition) >= 0.5f)
+        {
+            isMovingCamera = false;
+        }
+    }
+
+    private void MoveTargetElement()
+    {
+        Vector3 targetElementPosition = targetElement.position + Vector3.up * cameraSpeed * Time.deltaTime;
+        targetElement.position = targetElementPosition;
+    }
+
+    private void MoveCameraTrigger()
+    {
+        Vector3 triggerPosition = cameraTrigger.transform.position + Vector3.up * cameraSpeed * Time.deltaTime;
+        cameraTrigger.transform.position = triggerPosition;
+    }
+
+    private void MoveTetrisManager()
+    {
+        Vector3 tetrisManagerPosition = tetrisManager.transform.position + Vector3.up * cameraSpeed * Time.deltaTime;
+        tetrisManager.transform.position = tetrisManagerPosition;
+    }
+}
---
+++ Assets/Scripts/TetrisManager.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject[] tetrominoes;
+    public float spawnInterval = 1f;
+    public int minX = -15;
+    public int maxX = 15;
+
+    private GameObject currentTetromino;
+    private float spawnTimer;
+
+    private void Start()
+    {
+        spawnTimer = spawnInterval;
+        SpawnTetromino(null);
+    }
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
+        {
+            spawnTimer = spawnInterval;
+            SpawnTetromino(null);
+        }
+    }
+
+    private void SpawnTetromino(float? x)
+    {
+        if(x == null)
+        {
+            int index = UnityEngine.Random.Range(0, tetrominoes.Length);
+            int randomX = UnityEngine.Random.Range(minX, maxX);
+            var xSpawnPoint = randomX * (float)0.4;
+
+             Vector3 spawnPosition = new Vector3(xSpawnPoint, spawnPoint.position.y, spawnPoint.position.z);
+            currentTetromino = Instantiate(tetrominoes[index], spawnPosition, Quaternion.identity);
+
+            // Losowy wybór rotacji
+            var rotation = UnityEngine.Random.Range(0, 4) * 90;
+            int rotationIndex = UnityEngine.Random.Range(0, rotation) * 90;
+            currentTetromino.transform.rotation = Quaternion.Euler(new Vector3(spawnPoint.position.x, spawnPoint.position.y, rotationIndex));
+
+            DoubleBlockTrigger(xSpawnPoint);
+
+        }
+        else if(x != null)
+        {
+            int index = UnityEngine.Random.Range(0, tetrominoes.Length);
+            int randomX = UnityEngine.Random.Range(minX, maxX);
+            var xSpawnPoint = randomX * (float)0.4;
+            
+            if(xSpawnPoint - 1.0f < x && x < xSpawnPoint + 1.0f)
+            {
+                float randomNumber = UnityEngine.Random.Range(2f, 4f);
+                if (xSpawnPoint - 1.0f < x)
+                {
+                    xSpawnPoint += 1.0f*randomNumber;
+                }
+                else if(x < xSpawnPoint + 1.0f)
+                {
+                    xSpawnPoint -= 1.0f*randomNumber;
+                }
+            }
+
+            Vector3 spawnPosition = new Vector3(xSpawnPoint, spawnPoint.position.y, spawnPoint.position.z);
+            currentTetromino = Instantiate(tetrominoes[index], spawnPosition, Quaternion.identity);
+
+            // Losowy wybór rotacji
+            var rotation = UnityEngine.Random.Range(0, 4) * 90;
+            int rotationIndex = UnityEngine.Random.Range(0, rotation) * 90;
+            currentTetromino.transform.rotation = Quaternion.Euler(new Vector3(spawnPoint.position.x, spawnPoint.position.y, rotationIndex));
+        }
+        
+
+        
+    }
+
+    private void MoveTetromino(Vector3 direction)
+    {
+        currentTetromino.transform.position += direction;
+    }
+
+    private void RotateTetromino()
+    {
+        currentTetromino.transform.Rotate(Vector3.forward * 90f);
+        currentTetromino.transform.Rotate(Vector3.back * 90f);
+    }
+
+    private void DoubleBlockTrigger(float x)
+    {
+        int randomNumber = UnityEngine.Random.Range(0, 100);
+        if(randomNumber > 85)
+        {
+            SpawnTetromino(x);
+        }
+    }
+}
