6af207ed360b39920b565e67c7f822933f03c44a
filiprydzewski <efrydzewski@gmail.com>
2023-06-14 23:07:45 +0200

    menu i umieranie

--- Assets/Scripts/ObjectHealth.cs
+++ Assets/Scripts/ObjectHealth.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectHealth : MonoBehaviour
 {
     public Animator animator;
     public HealthBar healthBar;
-
+    public AudioSource soundEffect;
     public int maxHealth = 100;
     int currentHealth;
 
@@ -21,12 +22,26 @@ public class ObjectHealth : MonoBehaviour
     // change name to stun?
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
+        if (currentHealth > damage) {
+            currentHealth -= damage;
+        } else {
+            currentHealth = 0;
+        }
+       
         healthBar.SetHealth(currentHealth);
 
         if (currentHealth <= 0)
         {
-            Die();
+            AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+                    foreach (AudioSource source in audioSources)
+                    {
+                        soundEffect.Play();
+                        source.Stop();
+                        
+
+                    }
+                    Time.timeScale = 0f;
+                    StartCoroutine(WaitAndLoadNextScene(5f));
         }
     }
 
@@ -36,9 +51,18 @@ public class ObjectHealth : MonoBehaviour
         
         animator.SetBool("IsDead", true);
 
+        
+
+
         // nie działa mi to :(
         this.enabled = false;
 
     }
 
+    IEnumerator WaitAndLoadNextScene(float waitTime)
+{
+    yield return new WaitForSecondsRealtime(waitTime);
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    Time.timeScale = 1f;
+}
 }
